a02499cf48311db6b4ebf04b61792a859b02d570
phi <ficarde@gmail.com>
2021-03-09 18:58:28 -0300

    Add option to change stat increase values + Add option for the power tab to increase hit instead of the speed tab

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ $ asar patch.asm <chrono-trigger-rom.smc>
 This will modify the rom to be the patched rom, so you should use a copy.
 This is Jets of Time compatible, as long as you change the relevant setting in patch.asm to 1.
 
-There is also an option to increase exp/gold/tech points gain, which you can set by changing `!IncreaseExpGoldTech` to `1`. It multiplies experience by 4, gold by 8, and tech points by 4, and when you reach levels 60 and 80, experience gains grows to 8 and 16, respectively. I mostly added it to speed up rando runs. (I am not a fan of low level runs anyway, and I don't like not being able to buy all the stuff that's all in the shops)
+You can change the amount each tab increases each stat by changing the `!*Increase` settings, e.g. `!PowerIncrease = 3` will make the power tab increase 3 power. There is also an option to make the power tab increase hit instead of the speed tab, `!PowerTabIncreasesHit`, set it to 1 to enable it.
+
+There is an unrelated option to increase exp/gold/tech points gain, which you can enable by changing `!IncreaseExpGoldTech` to `1`. It multiplies experience by 4, gold by 8, and tech points by 4, and when you reach levels 60 and 80, experience gains grows to 8 and 16, respectively. I mostly added it to speed up rando runs. (I am not a fan of low level runs anyway, and I don't like not being able to buy all the stuff that's all in the shops) If it ever becomes a burden I might separate it into its own repo.
+
--- patch.asm
+++ patch.asm
@@ -4,7 +4,38 @@ hirom
 
 !IncreasedExpGoldTech = 0 ; 1 = 4x exp, 8x gold, 4x tech points
 !BetterTabs = 1 ; 1 = tabs increase more stats
+!PowerTabIncreasesHit = 0 ; power tab will increase hit instead of speed tab
 !JetsOfTimeRando = 0 ; 1 = compatible with jets of time randomizer, incompatible with normal game
+
+!PowerIncrease = 2
+!StaminaIncrease = 2
+!SpeedIncrease = 1
+!MagicIncrease = 2
+!HitIncrease = 2
+!EvadeIncrease = 2
+!MagicDefenseIncrease = 2
+
+; Setting Assertions
+
+if !PowerTabIncreasesHit != 0
+    !HitTab = "Power"
+else
+    !HitTab = "Speed"
+endif
+
+assert !PowerIncrease <= 99, "Power tab cannot increase more than 99 power"
+assert !PowerIncrease >= 0, "Power tab cannot decrease power"
+assert !StaminaIncrease <= 99, "Power tab cannot increase more than 99 stamina"
+assert !StaminaIncrease >= 0, "Power tab cannot decrease stamina"
+assert !SpeedIncrease <= 16, "Speed tab cannot increase more than 16 speed"
+assert !SpeedIncrease >= 0, "Speed tab cannot decrease speed"
+assert !MagicIncrease <= 99, "Magic tab cannot increase more than 99 magic"
+assert !HitIncrease <= 99, "!HitTab tab cannot increase more than 99 hit"
+assert !HitIncrease >= 0, "!HitTab tab cannot decrease hit"
+assert !EvadeIncrease <= 99, "Speed tab cannot increase more than 99 evade"
+assert !EvadeIncrease >= 0, "Speed tab cannot decrease evade"
+assert !MagicDefenseIncrease <= 99, "Magic tab cannot increase more than 99 magic defense"
+assert !MagicDefenseIncrease >= 0, "Magic tab cannot decrease magic defense"
 
 ; Increased Exp/Gold/Tech points
 
@@ -78,19 +109,49 @@ org $F75DDB
 else
 org $CC36C1
 endif
+
+
+!n = 0
+macro NumberToText(value)
+    !n #= !n+1
+    !{value!{n}} #= <value>
+    if !{value!{n}} >= 10
+       !{n!{n}} = !n
+       %NumberToText(safediv(!{value!{n!{n}}}, 10, 0))
+       !{value!{n}} #= !{value!{n}}%10
+    endif
+    db $D4+!{value!{n}}
+    !n #= !n-1
+endmacro
+
 PowerTabText:
-    db $AF, $62, $26, $EC, $D6, $E0, $B2, $CD
-    db $BA, $C6, $25, $BA, $EC, $D6, $00
+    db $AF, $62, $26, $EC ; Power+
+    %NumberToText(!PowerIncrease)
+    db $E0, $B2, $CD, $BA, $C6, $25, $BA, $EC ; /Stamina+
+    %NumberToText(!StaminaIncrease)
+if !PowerTabIncreasesHit != 0
+    db $E0, $A7, $2E, $EC ; /Hit+
+    %NumberToText(!HitIncrease)
+endif
+    db $00 ; Null
 
 MagicTabText:
-    db $AC, $BA, $C0, $C2, $BC, $EC, $D6, $E0
-    db $AC, $E8, $A3, $BE, $BF, $33, $5A, $EC
-    db $D6, $00
+    db $AC, $BA, $C0, $C2, $BC, $EC ; Magic+
+    %NumberToText(!MagicIncrease)
+    db $E0, $AC, $E8, $A3, $BE, $BF, $33, $5A, $EC ; /M.Defense+
+    %NumberToText(!MagicDefenseIncrease)
+    db $00 ; Null
 
 SpeedTabText:
-    db $B2, $C9, $6D, $BD, $EC, $D5, $E0, $A7
-    db $2E, $EC, $D6, $E0, $A4, $CF, $BA, $BD
-    db $BE, $EC, $D6, $00
+    db $B2, $C9, $6D, $BD, $EC ; Speed+
+    %NumberToText(!SpeedIncrease)
+if !PowerTabIncreasesHit == 0
+    db $E0, $A7, $2E, $EC ; /Hit+
+    %NumberToText(!HitIncrease)
+endif
+    db $E0, $A4, $CF, $BA, $BD, $BE, $EC ; /Evade+
+    %NumberToText(!EvadeIncrease)
+    db $00 ; Null
 
 D0ItemText:
     db $00
@@ -166,6 +227,12 @@ JML AddPoints.ReturnPoint
 endif
 
 if !BetterTabs != 0
+macro StatCondition(value, statOffset, max, allow)
+     if <value> > 0
+         LDA $9A9B+<statOffset> : CMP #<max> : BCC <allow>
+     endif
+endmacro
+
 TabCondition:
     CPY #$00 : BEQ .powerTab
     CPY #$02 : BEQ .speedTab
@@ -173,37 +240,35 @@ TabCondition:
     JML TabCondition.Disallow
 
     .powerTab
-    LDA $9A9B+00 : CMP #$63 : BCC .allow ; power
-    LDA $9A9B+01 : CMP #$63 : BCC .allow ; stamina
+    %StatCondition(!PowerIncrease,   00, $63, .allow)
+    %StatCondition(!StaminaIncrease, 01, $63, .allow)
+    if !PowerTabIncreasesHit != 0
+        %StatCondition(!HitIncrease, 04, $63, .allow)
+    endif
     JML TabCondition.Disallow
 
     .speedTab
-    LDA $9A9B+02 : CMP #$10 : BCC .allow ; speed
-    LDA $9A9B+04 : CMP #$63 : BCC .allow ; hit
-    LDA $9A9B+05 : CMP #$63 : BCC .allow ; evade
+    %StatCondition(!SpeedIncrease, 02, $10, .allow)
+    %StatCondition(!EvadeIncrease, 05, $63, .allow)
+    if !PowerTabIncreasesHit == 0
+        %StatCondition(!HitIncrease, 04, $63, .allow)
+    endif
     JML TabCondition.Disallow
 
     .magicTab
-    LDA $9A9B+03 : CMP #$63 : BCC .allow ; magic
-    LDA $9A9B+06 : CMP #$63 : BCC .allow ; magic defense
+    %StatCondition(!MagicIncrease,        03, $63, .allow)
+    %StatCondition(!MagicDefenseIncrease, 06, $63, .allow)
     JML TabCondition.Disallow
 
     .allow
 JML TabCondition.Allow
 
 
-macro IncreaseStatBy2(statOffset, max)
-    LDA <statOffset>, X : CMP #<max> : BCS ?afterStat
-        INC <statOffset>, X
-        CMP #(<max>-1) : BCS ?afterStat
-        INC <statOffset>, X
-    ?afterStat:
-endmacro
-
-macro IncreaseStatBy1(statOffset, max)
-    LDA <statOffset>, X : CMP #<max> : BCS ?afterStat
-        INC <statOffset>, X
-    ?afterStat:
+macro IncreaseStat(statOffset, max, by)
+    LDA <statOffset>, X : CLC : ADC #<by> : CMP #<max> : BCC ?setStat
+        LDA #<max>
+    ?setStat:
+    STA <statOffset>, X
 endmacro
 
 TabIncrease:
@@ -215,19 +280,24 @@ TabIncrease:
     JML TabIncrease.Return
 
     .powerTab
-    %IncreaseStatBy2($000B, $63) ; power
-    %IncreaseStatBy2($000C, $63) ; stamina
+    %IncreaseStat($000B, $63, !PowerIncrease)
+    %IncreaseStat($000C, $63, !StaminaIncrease)
+    if !PowerTabIncreasesHit != 0
+        %IncreaseStat($000F, $63, !HitIncrease)
+    endif
     JML TabIncrease.Return
 
     .speedTab
-    %IncreaseStatBy1($000D, $10) ; speed
-    %IncreaseStatBy2($000F, $63) ; hit
-    %IncreaseStatBy2($0010, $63) ; evade
+    %IncreaseStat($000D, $10, !SpeedIncrease)
+    %IncreaseStat($0010, $63, !EvadeIncrease)
+    if !PowerTabIncreasesHit == 0
+        %IncreaseStat($000F, $63, !HitIncrease)
+    endif
     JML TabIncrease.Return
 
     .magicTab
-    %IncreaseStatBy2($000E, $63) ; magic
-    %IncreaseStatBy2($0011, $63) ; magic defense
+    %IncreaseStat($000E, $63, !MagicIncrease)
+    %IncreaseStat($0011, $63, !MagicDefenseIncrease)
 JML TabIncrease.Return
 endif
 
